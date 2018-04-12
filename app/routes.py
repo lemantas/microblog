40f892e65b570b390d1961c53c4614ef1d0944c7
Mantas <mantas.levinas@gmail.com>
2018-04-12 20:27:32 +0300

    teAvatars added

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
+from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
@@ -30,7 +30,7 @@ def login():
 	if form.validate_on_submit():
 		user = User.query.filter_by(username=form.username.data).first()
 		if user is None or not user.check_password(form.password.data):
-			flash('Invalid username or passwrd')
+			flash('Invalid username or password')
 			return redirect(url_for('login'))
 		login_user(user, remember=form.remember_me.data)
 		next_page = request.args.get('next')
@@ -58,3 +58,12 @@ def register():
 		return redirect(url_for("login"))
 	return render_template("register.html", title="Register", form=form)
 
+@app.route("/user/<username>")
+@login_required
+def user(username):
+	user = User.query.filter_by(username=username).first_or_404()
+	posts = [
+		{"author": user, "body": "Test post #1"},
+		{"author": user, "body": "Test post #2"}
+	]
+	return render_template("user.html", user=user, posts=posts)
