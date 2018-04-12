04409b5a4af8c82d8759dcbb2d701e576755d786
Mantas <mantas.levinas@gmail.com>
2018-04-12 19:31:46 +0300

    User Registration implemented

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
@@ -10,7 +10,6 @@ from werkzeug.urls import url_parse
 @app.route('/index')
 @login_required
 def index():
-	user = {'username': 'Mantas'}
 	posts = [
 		{
 			'author': {'username': 'Marsietis'},
@@ -21,7 +20,7 @@ def index():
 			'body': 'The Avengers movie was so cool!'
 		}
 	]
-	return render_template('index.html', title='Home', user=user, posts=posts)
+	return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -44,3 +43,18 @@ def login():
 def logout():
 	logout_user()
 	return redirect(url_for('index'))	
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for("index"))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email=form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash("Congratulations, you are now registered! Enjoy the app.")
+		return redirect(url_for("login"))
+	return render_template("register.html", title="Register", form=form)
+
